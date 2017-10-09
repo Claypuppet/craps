610027cb49d2adb9ef8ddeab68f9ddf7cac7f86b
Jelle <jellebouwhuis@outlook.com>
2017-10-09 13:53:31 +0200

    maar deze

--- CrapsTest.cpp
+++ CrapsTest.cpp
@@ -17,7 +17,7 @@
 #include "Game.h"
 
 
-//#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_DYN_LINK
 
 
 #include <boost/test/unit_test.hpp>
--- main.cpp
+++ main.cpp
@@ -6,29 +6,24 @@
  */
 
 #include <iostream>
-
 #include "Game.h"
 
-
-#define BOOST_TEST_MODULE CrapsTestModule1
+#define BOOST_TEST_MODULE CrapsTestModule2
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_NO_MAIN
 
 #include <boost/test/unit_test.hpp>
-
 
 void playGame(){
 	Game g;
 	g.play();
 }
 
-
 int main(int argc, char **argv)
 {
 	std::cout << "Craps started" << std::endl << std::endl;
 
-
-	boost::unit_test::unit_test_main( &init_unit_test, argc, argv );
+	boost::unit_test::unit_test_main(&init_unit_test, argc, argv);
 //	playGame();
 
 	std::cout << std::endl << "Craps finished" << std::endl;
